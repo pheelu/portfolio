feca6d1f0f90a32396a5147db2a8ea2aa33cc75a
Filippo Alzati <filippoprivata@gmail.com>
2024-12-21 00:23:10 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,13 +5,53 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa'
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h2 className="my-20 text-center text-4xl">Contact</h2>
-        <div className="text-center tracking-tighter">
-            <p className="my-4">{CONTACT.phoneNo}</p>
-            <a href ="#" className="border-b">{CONTACT.email}</a>
+      <h2 className="my-10 text-center text-4xl">Contact</h2>
+      <div className="max-w-lg mx-auto border border-white rounded-xl p-6 mx-2">
+        <div className="flex justify-center mt-2 mb-8 space-x-4">
+          <motion.a
+            href="https://www.linkedin.com/in/filippo-alzati-a9428a215/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              whileHover={{
+                scale: 1.2,
+                transition: { duration: 0.3 },
+              }}
+            >
+              <FaLinkedin className="text-3xl sm:text-4xl hover:text-blue-500" />
+            </motion.div>
+          </motion.a>
+          <motion.a
+            href="https://github.com/pheelu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+              whileHover={{
+                scale: 1.2,
+                transition: { duration: 0.3 },
+              }}
+            >
+              <FaGithub className="text-3xl sm:text-4xl hover:text-gray-700" />
+            </motion.div>
+          </motion.a>
         </div>
+        <div className="text-center tracking-tighter">
+          <p className="my-4">{CONTACT.phoneNo}</p>
+          <a href="#" className="border-b border-white hover:border-neutral-300">
+            {CONTACT.email}
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
