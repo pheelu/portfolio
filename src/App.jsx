263df8f4f084f27b4aebe50c7c3d9760496d89cd
Filippo Alzati <filippoprivata@gmail.com>
2024-12-20 19:48:43 +0100

    nav-fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/navbar';
+
 import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
@@ -7,12 +7,13 @@ import Education from './components/Education';
 import Projects from './components/Projects';
 import SoftSkills from './components/SoftSkills';
 import Contact from './components/Contact';
+import NavigatorBar from './components/NavigatorBar';
 
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden bg-neutral-950 bg-[radial-gradient(ellipse_100%_100%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="sm:px-20 lg:px-32">
-        <Navbar />
+        <NavigatorBar />
         <Hero />
         <About />
         <Technologies />
