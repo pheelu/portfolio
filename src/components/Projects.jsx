322b41b0ece6db58a649e1cb50739314d80391b0
Filippo Alzati <filippoprivata@gmail.com>
2025-02-14 17:33:21 +0100

    diminish animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
         <motion.h2
             whileInView={{ opacity : 1, y : 0 }}
             initial={{ opacity : 0, y : -50 }}
-            transition = {{ duration : 0.5 }} 
+            transition = {{ duration : 0.2 }} 
             className="my-20 text-center text-4xl">
             Main{" "}
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent">
@@ -20,7 +20,7 @@ const Projects = () => {
                     <motion.div
                         whileInView={{ opacity : 1, x : 0 }}
                         initial={{ opacity: 0, x: -100 }}
-                        transition = {{ duration : 0.5 }} 
+                        transition = {{ duration : 0.2 }} 
                         className="w-full lg:w-1/4 px-4">
                         <img 
                         src={project.image}
@@ -33,7 +33,7 @@ const Projects = () => {
                     <motion.div
                         whileInView={{ opacity : 1, x : 0 }}
                         initial={{ opacity: 0, x: 100 }}
-                        transition = {{ duration : 0.5 }}  
+                        transition = {{ duration : 0.2 }}  
                         className="w-full max-w-xl lg:w-3/4">
                         <h6 className='mb-2 font-semibold'>{project.title}</h6>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
