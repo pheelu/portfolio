f0fcb966ec2e42450824814ca4e841001b094264
Filippo Alzati <filippoprivata@gmail.com>
2025-02-15 10:28:44 +0100

    hero animations fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,18 @@
 import { HERO_CONTENT } from '../constants';
-import logo from '../assets/falogo.png'
+import logo from '../assets/falogo.png';
 import { motion } from 'framer-motion';
 
 const container = (delay) => ({
-  hidden : {x: 0, opacity: 0},
-  visible : {
-    x: 0,
+  hidden: { opacity: 0 },
+  visible: {
     opacity: 1,
     transition: {
       duration: 0.5,
       delay: delay,
+      ease: 'easeInOut',
     }
   }
-})
+});
 
 const Hero = () => {
   return (
@@ -20,20 +20,17 @@ const Hero = () => {
       <div className="flex flex-wrap justify-center">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center text-center">
-          <motion.img
+            <motion.img
               src={logo}
               alt="logo"
-              className="w-40 h-40 mb-20 mt-10"
+              className="w-40 h-40 mb-16 mt-10"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 1 }}
-              whileHover={{ //Da fixare
-                scale: 1.1,
-                transition: { duration: 0.3 },
-              }}
+              whileHover={{ scale: 1.05 }}
             />
             <motion.span
-              variants={container(0.5)}
+              variants={container(0.3)}
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
@@ -41,10 +38,10 @@ const Hero = () => {
             </motion.span>
           </div>
           <motion.p
-            variants={container(1)}
+            variants={container(0.7)}
             initial="hidden"
-            animate="visible" 
-            className="my-2 mx-auto max-w-xl py-6 px-4 font-light tracking-tighter text-center">
+            animate="visible"
+            className="my-2 mx-auto max-w-xl py-4 px-4 font-light tracking-tighter text-center">
             {HERO_CONTENT}
           </motion.p>
         </div>
