fd696c7a2d8c31cda4ace33a5c53c971a2d0f902
Filippo Alzati <filippoprivata@gmail.com>
2025-02-15 11:15:37 +0100

    contact uodate v2

--- src/components/SoftSkills.jsx
+++ src/components/SoftSkills.jsx
@@ -1,63 +1,65 @@
-import { TiWorld } from "react-icons/ti";
-import { BsChatRightDots } from "react-icons/bs";
-import { TfiStatsUp } from "react-icons/tfi";
-import { motion } from "framer-motion";
+import { CONTACT } from '../constants';
+import { motion } from 'framer-motion';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
-const appearingProps = {
-  whileInView: { opacity: 1, y: 0 },
-  initial: { opacity: 0, y: -5 },
-  transition: { duration: 0.4 },
-};
-
-const SoftSkills = () => {
+const Contact = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <motion.h2 {...appearingProps} className="my-20 text-center text-4xl">
-        Soft{" "}
-        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent">
-          Skills
-        </span>
-      </motion.h2>
-      <div className="px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-12 justify-center mb-24 max-w-5xl mx-auto">
-          {[
-            {
-              icon: <TiWorld className="text-5xl text-purple-800" />,
-              title: "Languages",
-              text: "I speak 4 languages!\nItalian, English, Spanish, and French.",
-            },
-            {
-              icon: <BsChatRightDots className="text-5xl text-purple-800" />,
-              title: "Communication",
-              text: "I have worked as a waiter, interacting with customers and learning how to engage effectively with people.",
-            },
-            {
-              icon: <TfiStatsUp className="text-5xl text-purple-800" />,
-              title: "Adaptability",
-              text: "I strive to stay updated and expand my knowledge, such as this portfolio.",
-            },
-          ].map((skill, index) => (
-            <div key={index} className="flex sm:flex-col flex-row items-center sm:space-y-4 space-x-4 sm:space-x-0">
+    <div className="border-b border-neutral-900 pb-20">
+      <h2 className="my-10 text-center text-4xl">Contact</h2>
+      <div className="max-w-lg mx-auto p-6 mx-2 relative">
+        <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 p-1 -z-10">
+          <div className="bg-neutral-900 rounded-lg h-full w-full"></div>
+        </div>
+        <div className="relative">
+          <div className="flex justify-center mt-2 mb-8 space-x-4">
+            <motion.a
+              href="https://www.linkedin.com/in/filippo-alzati-a9428a215/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <motion.div
-                {...appearingProps}
-                className="rounded-2xl border-4 border-neutral-800 p-6 flex items-center justify-center"
+                initial={{ opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 },
+                }}
               >
-                {skill.icon}
+                <FaLinkedin className="text-3xl sm:text-4xl hover:text-blue-500" />
               </motion.div>
-              <div>
-                <motion.h3 {...appearingProps} className="mt-2 text-center font-semibold text-xl">
-                  {skill.title}
-                </motion.h3>
-                <motion.p {...appearingProps} className="text-center text-sm mt-2">
-                  {skill.text}
-                </motion.p>
-              </div>
-            </div>
-          ))}
+            </motion.a>
+            <motion.a
+              href="https://github.com/pheelu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 },
+                }}
+              >
+                <FaGithub className="text-3xl sm:text-4xl hover:text-gray-700" />
+              </motion.div>
+            </motion.a>
+          </div>
+          <div className="text-center tracking-tighter">
+            <p className="my-4">{CONTACT.phoneNo}</p>
+            <a
+              href={`mailto:${CONTACT.email}`} // Opens default email client
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent font-medium hover:opacity-80 transition-opacity duration-300"
+            >
+              {CONTACT.email}
+            </a>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default SoftSkills;
+export default Contact;
