133b47424f727126e2bd6c2a96a3bdbc7bbde4be
Filippo Alzati <filippoprivata@gmail.com>
2025-02-17 11:56:33 +0100

    cv rm

--- src/components/NavigatorBar.jsx
+++ src/components/NavigatorBar.jsx
@@ -1,8 +1,7 @@
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
-import { FaFileDownload } from 'react-icons/fa'; // Icona per il download
 import { motion } from 'framer-motion';
-import CV from '../assets/Filippo_Alzati_CV.pdf'; // Importa il file del CV
+
 
 const NavigatorBar = () => {
   return (
@@ -49,25 +48,6 @@ const NavigatorBar = () => {
             }}
           >
             <FaGithub className="text-3xl sm:text-4xl hover:text-gray-700" />
-          </motion.div>
-        </motion.a>
-        {/* Pulsante di download del CV */}
-        <motion.a
-          href={CV} // Usa la variabile CV importata
-          download="Filippo_Alzati_CV.pdf" // Nome del file che verrà scaricato
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <motion.div
-            initial={{ x: +100, opacity: 0 }} // Initial state: off-screen and invisible
-            animate={{ x: 0, opacity: 1 }}    // Animate to on-screen and visible
-            transition={{ duration: 0.5, delay: 0.5 }} // Smooth transition with delay
-            whileHover={{
-              scale: 1.2, // Enlarge on hover
-              transition: { duration: 0.3 },
-            }}
-          >
-            <FaFileDownload className="text-3xl sm:text-4xl hover:text-green-500" />
           </motion.div>
         </motion.a>
       </div>
